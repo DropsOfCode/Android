fcde15bbe146ae80d39636029703ca520c5d8166
DropsOfCode <1095941373@qq.com>
2018-10-14 11:27:04 +0800

    ListView

---
+++ ListView/ListView(Efficiency)/1st/PersonAdapter.java
@@ -0,0 +1,51 @@
+package com.example.bin.hell1;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonAdapter extends ArrayAdapter {
+    private int resourceId;
+
+    //重写构造函数，将resourceId引进来
+    public PersonAdapter(Context context,int resourceId,List<Person> people){
+        super(context,resourceId,people);
+        this.resourceId = resourceId;
+    }
+
+    //重写getView方法
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Person p = (Person)getItem(position); //getItem方法可获得由构造函数拿到的数据
+        View view;
+        ViewHolder viewHolder;
+        if(convertView==null){ //如果是第一次加载布局，则先要加载布局
+            view = LayoutInflater.from(getContext()).inflate(this.resourceId,parent,false);
+            viewHolder = new ViewHolder();
+            viewHolder.imageView = (ImageView)view.findViewById(R.id.names_image);
+            viewHolder.textView = (TextView) view.findViewById(R.id.names_text);
+            view.setTag(viewHolder);
+        }else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        viewHolder.imageView.setImageResource(p.getImageId()); //设置图片
+        viewHolder.textView.setText(p.getName()); //设置文本
+        return view; //返回视图
+    }
+
+    class ViewHolder{
+        ImageView imageView;
+        TextView textView;
+    }
+}
