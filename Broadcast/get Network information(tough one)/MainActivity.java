1e3b8b17190ddc057ad55edb0298ce87dd9f9370
DropsOfCode <1095941373@qq.com>
2018-10-15 12:42:44 +0800

    Broadcast

--- Broadcast/get Network information(tough one)/MainActivity.java
+++ Broadcast/get Network information(tough one)/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -16,19 +17,24 @@ public class MainActivity extends AppCompatActivity {
     private NetworkChangeReceiver networkChangeReceiver;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         intentFilter = new IntentFilter();
+        /*系统网络状态发生变化时，系统发出的是android.net.conn.CONNECTIVITY_CHANGE的广播
+        * 要监听什么样的广播，就要添加相应的action*/
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
 
-
         networkChangeReceiver = new NetworkChangeReceiver();
+        /*注册广播，第一个参数是要接收的接收器，第二个参数是指要监听的内容*/
         registerReceiver(networkChangeReceiver,intentFilter);
     }
 
+
+    /*
+    * 动态注册的广播一定都要取消注册，取消注册采用unregisterReceiver方法
+    * */
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -38,13 +44,16 @@ public class MainActivity extends AppCompatActivity {
     class NetworkChangeReceiver extends BroadcastReceiver{
         @Override
         public void onReceive(Context context, Intent intent) {
-            ConnectivityManager connectionManager = (ConnectivityManager)getSystemService(context.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
-
-            if(networkInfo!=null&&networkInfo.isAvailable()){
-                Toast.makeText(context, "network is working", Toast.LENGTH_SHORT).show();
+            /*
+            * ConnectivityManager是专门管理网络的一个类
+            * ConnectivityManager是可以通过getSystemService获得相应的实例，获得的条件是getSystemService参数设置为CONNECTIVITY_SERVICE
+            * */
+            ConnectivityManager connectionManager =  (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo(); //之后通过NetworkInfo来获得系统网络的信息（要注意去Manifest去开启权限）
+            if(networkInfo!=null&&networkInfo.isAvailable()){ //当不为空且信息可用时
+                Toast.makeText(context, "Network is working", Toast.LENGTH_SHORT).show();
             }else{
-                Toast.makeText(context,"network is shut down",Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "Network is shut down", Toast.LENGTH_SHORT).show();
             }
         }
     }
